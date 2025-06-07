64c998ba51596854363c4cc232e9a933ce82c056
Godhelpsme <qzh040411@gmail.com>
2025-06-08 01:50:01 +0800

    Update proxy.ts
    添加duplex: 'half'选项

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -69,6 +69,7 @@ export default async (request: Request, context: Context) => {
     body: request.body,
     method: request.method,
     headers,
+    ...(request.body && { duplex: 'half' } as any)
   });
 
   const responseHeaders = {
